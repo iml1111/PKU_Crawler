eb0a7f83fd57528f244e0c47752437e445cf7f72
iml1111 <shin10256@gmail.com>
2018-05-18 04:34:04 +0900

    second

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-from urllib.request import FancyURLopener, HTTPError
-import re
-import sys
-import os
-
+"""	        PKU_Crwaler	"""
+"""	     	  BY IML  	"""
+"""	shin10256|gmail.com   	"""	
+"""	shino1025.blog.me    	"""
+"""	github.com/iml1111   	"""
 from url_list import List
+from crawl_select import Crawling
+from url_parser import URLparser
 
-class AppURLopener(FancyURLopener):     			 
-   	version = "Mozilla/5.0"
-
-def URLparser(URL):
-	try:
-		html = AppURLopener().open(URL)
-	except HTTPError as e:
-		print(e)
-		print("[*] HTTP ERROR!")
-		sys.exit(1)
+URL = List[0]  #for Debug 
 
 if __name__ == '__main__':
-	for i in List:
-		print(i['url'])
-		print(i['info'])
+	
+	html = URLparser(URL['url'])
+	Crawling(html, URL['info'])
+		
